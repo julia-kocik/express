895b89477c504cd134ecdbcd16435699c44b6024
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-07-14 19:28:27 +0200

    Add changes2

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', (req, res) => {
   res.show('index.html');
 });
@@ -30,13 +32,6 @@ app.get('/history', (req, res) => {
   res.show('history.html');
 });
 
-app.get('/style.css', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/style.css'));
-  });
-    
-  app.get('/test.jpg', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/test.jpg'));
-  });
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
